9a9c08e0f7b57fbf69b8d67e46c2a2c63cda995f
kariray <kari_578@naver.com>
2022-01-06 20:36:48 +0900

    movie view

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from movies.views import MoviesView
+from movies.views import MoviesView, MovieDetail, UpdateMovie, CreateMovie, DeleteMovie
 
 app_name = "movies"
 
 urlpatterns = [
     path("", MoviesView.as_view(), name="movies"),
+    path("<int:pk>", MovieDetail.as_view(), name="movie"),
+    path("<int:pk>/update", UpdateMovie.as_view(), name="update"),
+    path("create/", CreateMovie.as_view(), name="create"),
+    path("<int:pk>/delete", DeleteMovie.as_view(), name="delete"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
-from django.views.generic import ListView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from django.shortcuts import render
 from movies.models import Movie
 
@@ -7,8 +8,58 @@ from movies.models import Movie
 
 
 class MoviesView(ListView):
+    """Show the Movie lists"""
+
     model = Movie
     paginate_by = 10
     paginate_orphans = 5
     ordering = "-created_at"
     context_object_name = "movies"
+
+
+class MovieDetail(DetailView):
+    """ Show the movie details"""
+    model = Movie
+    context_object_name = "movie"
+
+
+class UpdateMovie(UpdateView):
+    """ Edit Movie's information """
+    model = Movie
+    fields = (
+        "title",
+        "year",
+        "cover_image",
+        "rating",
+        "category",
+        "director",
+        "cast",
+    )
+
+    def get_success_url(self):
+        movie_pk = self.kwargs.get("pk")
+        print(movie_pk)
+        return reverse("movies:movie", kwargs={"pk": movie_pk})
+
+
+class CreateMovie(CreateView):
+    """ Create Movie's information """
+    model = Movie
+    fields = (
+        "title",
+        "year",
+        "cover_image",
+        "rating",
+        "category",
+        "director",
+        "cast",
+    )
+
+    def get_success_url(self):
+        return reverse("movies:movies")
+
+
+class DeleteMovie(DeleteView):
+    """Delete Movie's information"""
+    model = Movie
+    success_url = reverse_lazy("movies:movies")
