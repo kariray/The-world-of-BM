7d10e7520a3bd698a6f55ba7fe444b816329a419
kariray <kari_578@naver.com>
2022-01-18 23:28:12 +0900

    popup background blur

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,17 +1,21 @@
 const deleteBtn = document.querySelector("#delete_btn");
 const popup = document.querySelector(".popup");
 const body = document.body;
+const detail = document.querySelector("#detail");
 
 deleteBtn.addEventListener("click", (e) => {
   popup.classList.toggle("show-popup");
   if (popup.classList.contains("show-popup")) {
     body.classList.add("scroll-hidden");
+    detail.classList.add("blur");
   } else {
     body.classList.remove("scroll-hidden");
+    detail.classList.remove("blur");
   }
 });
 
 function ClosePopup() {
   popup.classList.remove("show-popup");
   body.classList.remove("scroll-hidden");
+  detail.classList.remove("blur");
 }
