ecab9932302457887f2b9a5ea437452ee876c3fe
kariray <kari_578@naver.com>
2022-02-06 00:31:41 +0900

    login, logout function

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from users.views import LoginView
+from users.views import LoginView, logout_view
 
 app_name = "users"
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,16 @@
-from audioop import reverse
-from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from users.forms import LoginForm
 
 # Create your views here.
+
+
+def logout_view(request):
+    """Logout"""
+    logout(request)
+    return redirect(reverse("core:home"))
 
 
 class LoginView(View):
@@ -21,7 +27,12 @@ class LoginView(View):
             email = form.cleaned_data.get("email")
             password = form.cleaned_data.get("password")
             user = authenticate(request, username=email, password=password)
+            print("start")
+            print(f"{email}, {password}")
+            print(user)
             if user is not None:
                 login(request, user)
+                print("user")
                 return redirect(reverse("core:home"))
+
         return render(request, "users/login.html", {"form": form})
